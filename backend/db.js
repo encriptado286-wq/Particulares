568224cbef474fc3ebd71bf2f8dcd11deca09388
Edrian <encriptado286@gmail.com>
2025-08-16 02:11:45 -0300

    shh

--- backend/db.js
+++ backend/db.js
@@ -3,8 +3,16 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // Render injecta esta variable automáticamente
-  ssl: { rejectUnauthorized: false },
+  user: 'particulares_db_user',          // tu usuario
+  host: 'dpg-d2f3m6jipnbc739miut0-a',   // internal hostname de Render
+  database: 'particulares_db',           // nombre de la DB
+  password: '91j9xuN3nsZK1KjHqSxVVVK5iJjH1623', // contraseña
+  port: 5432,
+  ssl: { rejectUnauthorized: false },     // obligatorio en Render
 });
+
+pool.connect()
+  .then(() => console.log('✅ Conectado a la base de datos'))
+  .catch(err => console.error('❌ Error conectando a la base de datos:', err));
 
 export default pool;
