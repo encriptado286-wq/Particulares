bf0883a3bf86542ae77d2e0c2bdbc819e30f3286
Edrian <encriptado286@gmail.com>
2025-08-15 22:48:51 -0300

    back

--- backend/index.js
+++ backend/index.js
@@ -27,3 +27,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Servidor en puerto ${PORT}`);
 });
+
+app.get('/', (req, res) => {
+  res.send('Servidor backend funcionando 🚀');
+});
